fe9c2cb47d0b44a02ec11ed0177022d49f187b46
jhall72 <46014270+jhall72@users.noreply.github.com>
2025-10-14 01:49:58 -0400

    Package ID

--- SpotifyCleaner/obj/Release/net9.0/SpotifyCleaner.AssemblyInfo.cs
+++ SpotifyCleaner/obj/Release/net9.0/SpotifyCleaner.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("A library for cleaning Spotify playlists and removing duplicates")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76bd0134bc77ad43e16e5b0eb287f98b5f0f8201")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f1a626d955babd4e8fc642dc68d369c0e38a493")]
 [assembly: System.Reflection.AssemblyProductAttribute("SpotifyCleaner")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SpotifyCleaner")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
